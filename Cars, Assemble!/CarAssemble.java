86a125176377bda2e558626ca4c0f453afd810c3
JakeWhrlWnd <justjamescreative@outlook.com>
2023-10-21 19:40:32 -0400

    Created the productionRatePerHour method that calculates the production rate per hour

--- Cars, Assemble!/CarAssemble.java
+++ Cars, Assemble!/CarAssemble.java
@@ -2,4 +2,26 @@
  * Use code to analyze the production of an assembly line in a car factory.
  */
 public class CarAssemble {
+    /**
+     * A method that considers the current assembly line's speed and calculates the production rate per hour
+     * At its lowest speed(1), 221 cars are produced each hour
+     *
+     * @param speed - int
+     * @return a double value that represents the Production Rate Per Hour
+     */
+    public double productionRatePerHour(int speed) {
+        if (speed == 10) {
+            //Speed 10 - 77% success rate
+            return speed * (221 * 0.77);
+        } else if (speed == 9) {
+            //Speed 9 - 80% success rate
+            return speed * (221 * 0.8);
+        } else if (speed >= 5 && speed <= 8) {
+            //Speed 5 to 8 - 90% success rate
+            return speed * (221 * 0.9);
+        } else {
+            //Speed 1 to 4 - 100% success rate
+            return speed * 221;
+        }
+    }
 }
