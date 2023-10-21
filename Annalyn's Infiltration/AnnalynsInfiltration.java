9b798aa94cb40ac0938f851e02fa35afaa16c8c4
JakeWhrlWnd <justjamescreative@outlook.com>
2023-10-21 05:54:49 -0400

    Implemented the static method canFastAttack()

--- Annalyn's Infiltration/AnnalynsInfiltration.java
+++ Annalyn's Infiltration/AnnalynsInfiltration.java
@@ -1,17 +1,16 @@
+/**
+ * Implementation of the quest logic for a RPG game
+ *
+ */
 public class AnnalynsInfiltration {
+    /**
+     * Fast attack is only available when the knight is sleeping
+     *
+     * @param knightIsAwake - negated boolean
+     * @return If you can execute a fast attack - boolean
+     */
     public static boolean canFastAttack(boolean knightIsAwake) {
         return !knightIsAwake;
     }
 
-    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
-    }
-
-    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        return !archerIsAwake && prisonerIsAwake;
-    }
-
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        return (!knightIsAwake && !archerIsAwake && prisonerIsAwake) || (!archerIsAwake && petDogIsPresent);
-    }
 }
