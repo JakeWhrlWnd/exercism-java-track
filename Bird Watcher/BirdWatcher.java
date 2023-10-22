3c69e8c7663e71b8b02a04cbad476f686fb43ffe
JakeWhrlWnd <justjamescreative@outlook.com>
2023-10-22 01:12:13 -0400

    Created the BirdWatcher.java Class

---
+++ Bird Watcher/BirdWatcher.java
@@ -0,0 +1,7 @@
+/**
+ * Create a class to keep track of how many birds have visited your garden in the last seven days
+ * Using Arrays, for-each loops, and for loops
+ * There are six tasks in this module
+ */
+public class BirdWatcher {
+}
