11d3623ec462cde7a95dfa481d83247ad1612bc8
JakeWhrlWnd <justjamescreative@outlook.com>
2023-10-22 01:27:30 -0400

    Declared the Array birdsPerDay Created the Constructor for the Class Completed the first task by creating the getLastWeek() method which checks the count from last week

--- Bird Watcher/BirdWatcher.java
+++ Bird Watcher/BirdWatcher.java
@@ -4,4 +4,17 @@
  * There are six tasks in this module
  */
 public class BirdWatcher {
+    private final int[] birdsPerDay; //Declaration of Array
+    //Constructor for the BirdWatcher Class
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    /**
+     * Check what the counts were last week
+     * @return last weeks count as an Array
+     */
+    public int[] getLastWeek() {
+        return this.birdsPerDay;
+    }
 }
